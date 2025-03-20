10b70c32c023254d20651c61f43baa332a30bc93
Lucas <lucasroveder@hotmail.com>
2025-03-20 17:18:30 -0300

    Arvore AVL --TO DO: método remove

---
+++ src/AVL.java
@@ -0,0 +1,234 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class AVL {
+    //creating the node class
+    public class Node{
+        private int value;
+        private Node left;
+        private Node right;
+        private int height;
+
+        //constructor
+        public Node(int value){
+            this.value = value;
+        }
+
+        //getter
+        public int getValue(){
+            return value;
+        }
+    }
+
+    private Node root;
+
+
+    public AVL(){
+
+    }
+    public Node getRoot() {
+        return root;
+    }
+
+    //height method
+    public int height() {
+        return height(root);
+    }
+
+    private int height(Node node) {
+        if (node == null) {
+            return -1;
+        }
+        return node.height;
+    }
+
+    //insertion method
+    public void insert(int value){
+        root = insert(value, root);
+    }
+
+    private Node insert(int value, Node node){
+        if(node == null){
+            node = new Node(value);
+            return node;
+        }
+        if(value < node.getValue()){
+            node.left = insert(value, node.left);
+        }
+        else if(value > node.getValue()){
+            node.right = insert(value, node.right);
+        }
+        node.height = Math.max(height(node.left), height(node.right)) + 1;
+        return rotate(node);
+    }
+
+    //rotations
+    private Node rotate(Node node) {
+        if (height(node.left) - height(node.right) > 1) {
+            // left heavy
+            if (height(node.left.left) - height(node.left.right) > 0) {
+                // left left case
+                return rightRotate(node);
+            }
+            if (height(node.left.left) - height(node.left.right) < 0) {
+                // left right case
+                node.left = leftRotate(node.left);
+                return rightRotate(node);
+            }
+        }
+
+        if (height(node.left) - height(node.right) < -1) {
+            // right heavy
+            if (height(node.right.left) - height(node.right.right) < 0) {
+                // right right case
+                return leftRotate(node);
+            }
+            if (height(node.right.left) - height(node.right.right) > 0) {
+                // left right case
+                node.right = rightRotate(node.right);
+                return leftRotate(node);
+            }
+        }
+        return node;
+    }
+
+    public Node rightRotate(Node p) {
+        Node c = p.left;
+        Node t = c.right;
+
+        c.right = p;
+        p.left = t;
+
+        p.height = Math.max(height(p.left), height(p.right) + 1);
+        c.height = Math.max(height(c.left), height(c.right) + 1);
+
+        return c;
+    }
+
+    public Node leftRotate(Node c) {
+        Node p = c.right;
+        Node t = p.left;
+
+        p.left = c;
+        c.right = t;
+
+        p.height = Math.max(height(p.left), height(p.right) + 1);
+        c.height = Math.max(height(c.left), height(c.right) + 1);
+
+        return p;
+    }
+
+    public void populate(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            this.insert(nums[i]);
+        }
+    }
+
+    public void populatedSorted(int[] nums) {
+        populatedSorted(nums, 0, nums.length);
+    }
+
+    private void populatedSorted(int[] nums, int start, int end) {
+        if (start >= end) {
+            return;
+        }
+
+        int mid = (start + end) / 2;
+
+        this.insert(nums[mid]);
+        populatedSorted(nums, start, mid);
+        populatedSorted(nums, mid + 1, end);
+    }
+
+    public void display() {
+        display(this.root, "Root Node: ");
+    }
+
+    private void display(Node node, String details) {
+        if (node == null) {
+            return;
+        }
+        System.out.println(details + node.value);
+        display(node.left, "Left child of " + node.value + " : ");
+        display(node.right, "Right child of " + node.value + " : ");
+    }
+
+    public boolean isEmpty() {
+        return root == null;
+    }
+
+    public boolean balanced() {
+        return balanced(root);
+    }
+
+    private boolean balanced(Node node) {
+        if (node == null) {
+            return true;
+        }
+        return Math.abs(height(node.left) - height(node.right)) <= 1 && balanced(node.left) && balanced(node.right);
+    }
+
+    public List<Integer> preOrder() {
+        List<Integer> list = new ArrayList<>();
+        preOrder(root, list);
+        return list;
+    }
+
+
+    private void preOrder(Node node, List<Integer> result) {
+        if (node != null) {
+            result.add(node.value);
+            preOrder(node.left, result);
+            preOrder(node.right, result);
+        }
+    }
+
+    public List<Integer> inOrder() {
+        List<Integer> list = new ArrayList<>();
+        inOrder(root, list);
+        return list;
+    }
+
+    private void inOrder(Node node, List<Integer> result) {
+        if (node != null) {
+            inOrder(node.left, result);
+            result.add(node.value);
+            inOrder(node.right, result);
+        }
+    }
+
+    public List<Integer> posOrder() {
+        List<Integer> list = new ArrayList<>();
+        posOrder(root, list);
+        return list;
+    }
+
+    private void posOrder(Node node, List<Integer> result) {
+        if (node != null) {
+            posOrder(node.left, result);
+            posOrder(node.right, result);
+            result.add(node.value);
+        }
+    }
+
+    public boolean search (int value){
+        return search(root, value);
+    }
+
+
+
+    public boolean search (Node node, int value) {
+        if (node != null) {
+            if (node.value == value) {
+                return true;
+            }
+            else if (value < node.value) {
+                return search(node.left, value);
+
+            }else return search(node.right, value);
+
+        }
+        return false;
+    }
+
+}
---
+++ src/Main.java
@@ -0,0 +1,53 @@
+import java.util.Arrays;
+import java.util.List;
+
+class Main {
+    public static void main(String[] args) {
+        AVL tree = new AVL();
+        tree.insert(4);
+        tree.insert(15);
+        tree.insert(23);
+        tree.insert(67);
+        tree.insert(89);
+        tree.insert(56);
+        tree.insert(14);
+        tree.insert(7);
+        tree.insert(-4);
+
+        List<Integer> elementsInOrder =  (List<Integer>) tree.inOrder();
+        List<Integer> elementsInOrderTest =  Arrays.asList(-4, 4, 7, 14, 15, 23, 56, 67, 89);
+        if(elementsInOrder.equals(elementsInOrderTest))
+            Log.isCorrect(" Inorder ", "São Iguais");
+        else Log.isError("Inorder ", "Diferente");
+
+
+        List<Integer> elementsPosOrder =  (List<Integer>) tree.posOrder();
+        List<Integer> elementsPosOrderTest =  Arrays.asList(-4, 7, 4, 15, 14, 56, 89, 67, 23);
+        if(elementsPosOrder.equals(elementsPosOrderTest))
+            Log.isCorrect(" PosOrder ", "São Iguais");
+        else Log.isError("PosOrder ", "Diferente");
+
+
+        List<Integer> elementsPreOrder =  (List<Integer>) tree.preOrder();
+        List<Integer> elementsPreOrderTest =  Arrays.asList(23, 14, 4, -4, 7, 15, 67, 56, 89);
+        if(elementsPreOrder.equals(elementsPreOrderTest))
+            Log.isCorrect(" PreOrder ", "São Iguais");
+        else Log.isError("PreOrder ", "Diferente");
+
+        System.out.println(tree.getRoot().getValue());
+        System.out.println(tree.search(tree.getRoot(), 56));
+
+/*        tree.remove(89);
+        elementsPreOrder =  (List<Integer>) tree.preOrder();
+        elementsPreOrderTest =  Arrays.asList(23, 14, 4, -4, 7, 15, 67, 56);
+        if(elementsPreOrder.equals(elementsPreOrderTest))
+            Log.isCorrect(" Remocao ", "OK");
+        else Log.isError("Remocao ", "erro");
+
+        if(!tree.search(45))
+            Log.isCorrect(" Busca ", "OK");
+        else  Log.isError("Busca ", "erro");
+
+ */
+    }
+}
